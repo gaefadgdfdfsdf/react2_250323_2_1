2103b1107551a354154910537708103bcc67ade1
seikSBS <seik1224@naver.com>
2025-03-23 10:32:36 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Step01 from './step/step01';
+import Step02 from './step/step02';
 
 function App() {
   return (
-    <Step01 />
+    <>
+      {/* <Step01 /> */}
+      <Step02 />
+    </>
   );
 }
 
---
+++ src/step/step02.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+/*
+    useRef사용법2. 애니메이션 제어
+*/
+const Step02 = () => {
+    return (
+        <div>Step02</div>
+    )
+}
+
+export default Step02
