3fa3d1afe11b3da58c1ecaeb6dc8e22618f9a041
seikSBS <seik1224@naver.com>
2025-03-23 11:13:09 +0900

    .

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Step02 from './step/step02';
 import Step03 from './step/step03';
 import Step04 from './step/step04';
 import Step05 from './step/step05';
+import Step06 from './step/step06';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       {/* <Step02 /> */}
       {/* <Step03 /> */}
       {/* <Step04 /> */}
-      <Step05 />
+      {/* <Step05 /> */}
+      <Step06 />
     </>
   );
 }
