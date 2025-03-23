456a933fcc92aea29eb255e91c86155a8d2bb43c
seikSBS <seik1224@naver.com>
2025-03-23 10:40:43 +0900

    .

--- src/step/step03.js
+++ src/step/step03.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const Step03 = () => {
+    const inputRef = useRef(null);
+
+    const focusInput = () => {
+        if(inputRef.current){
+            inputRef.current.focus();
+        }
+    }
     return (
-        <div>Step03</div>
+        <>
+            <input ref={inputRef} type='text' className='outline outline-1' />
+            <button onClick={focusInput}>CLICK</button>
+        </>
     )
 }
 
